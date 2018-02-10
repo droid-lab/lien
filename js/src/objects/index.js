a14946204503ecdb0a66449c4fb70d77f188a95b
Diogo Cezar <diogo@diogocezar.com>
2018-02-10 09:19:02 -0200

    Carroseis e Contato

--- js/src/objects/index.js
+++ js/src/objects/index.js
@@ -100,9 +100,33 @@ Index = {
 			}
 		}
 	},
+	Carousels: {
+		configs: {
+			'default': {
+				loop               : true,
+				nav                : false,
+				pagination         : true,
+				items              : 1,
+				dots               : true,
+				autoplay           : true,
+				autoplayTimeout    : 3000,
+				autoplayHoverPause : true,
+				singleItem         : true,
+				navText            : ["<i class='nav-left'></i>", "<i class='nav-right'></i>"]
+			}
+		},
+		init: function () {
+			$('.carousel-default').owlCarousel(Index.Carousels.configs['default']);
+			$('.carousel-default').on('mouseout', function () {
+				$('.carousel-default').trigger('stop.owl.autoplay');
+				$('.carousel-default').trigger('play.owl.autoplay', [3000]);
+			});
+		}
+	},
     init: function(){
     	Index.Menu.init();
-    	Index.Stellar.init();
+		Index.Stellar.init();
+		Index.Carousels.init();
     }
 }
 
